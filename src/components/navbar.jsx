038ab1ec4189d416906b0ef28e15076c75fd4a92
ngocduan99 <chrishoan99@gmail.com>
2022-08-14 13:23:05 +0700

    Creating the index

---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function navbar() {
+  return (
+    <div className='navbar--container'>
+        <div className="navbar--left"></div>
+        
+        <div className="navbar--right"></div>
+    </div>
+  )
+}
+
+export default navbar
