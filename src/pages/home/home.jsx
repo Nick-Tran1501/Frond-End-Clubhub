069db84ea7db56dcbcae7553cba7818139048479
ngocduan99 <chrishoan99@gmail.com>
2022-08-17 12:44:19 +0700

    Responsive homepage using Ant

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -5,18 +5,43 @@ import Feed from "../../components/feed/feed";
 import Rightbars from "../../components/rightbar/rightbar";
 import Footer from "../../components/footer/footer";
 import "./home.css";
-import { Layout } from "antd";
 import "antd/dist/antd.css";
+import { Col, Row } from 'antd';
 
-const { Header, Content, Sider } = Layout;
 
 function home() {
   return (
     <div className="homeContainer">
-      <Navbars/>
-      <Sidebars/>
-      <Feed/>
-      <Rightbars/>
+            <Row>
+        <Col span={24}>
+          <Navbars/>
+        </Col>
+      </Row>
+
+      <Row gutter={{
+        xs: 8,
+        sm: 16,
+        md: 24,
+        lg: 32,
+      }}>
+        <Col span={4}>
+          <Sidebars/>
+        </Col>
+
+        <Col xs={20} sm={14} md={14} lg={14} xl={14}>
+          <Feed/>
+        </Col>
+
+        <Col xs={0} sm={6} md={6} lg={6} xl={6}>
+          <Rightbars/>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col span={24}>
+          <Footer/>
+        </Col>
+      </Row>
     </div>
   );
 }
