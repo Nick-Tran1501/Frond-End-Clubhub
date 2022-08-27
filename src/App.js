81ec9e3eef8afe5982151f1dd14fd4d0cfdd405d
Anh Tuan <62132057+Andy1703@users.noreply.github.com>
2022-08-27 09:46:16 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,50 +1,26 @@
-import './App.css';
-import 'antd/dist/antd.css'
-import LoginPage from './Login/Login';
-import Home from './pages/home/home';
-import Welcome from './pages/welcome/welcome';
+import "./App.css";
+import "antd/dist/antd.css";
+import LoginPage from "./Login/Login";
+import Home from "./pages/home/home";
+import Welcome from "./pages/welcome/welcome";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Profile from './pages/profile/profile'
-import {Route,Routes, Router,Link} from "react-router-dom"
-import Register from './Login/Register';
+import Profile from "./pages/profile/profile";
+import { Route, Routes, Router, Link } from "react-router-dom";
+import Register from "./Login/Register";
 
 function App() {
-
- 
-
   return (
     <div className="App">
-          <Routes>
+      {/* <Routes>
             <Route path='/*' element={<LoginPage/>}/>
-            {/* <Route path='/register' element={<Register/>}/> */}
+            <Route path='/register' element={<Register/>}/>
             <Route path='/welcome' element={<Welcome/>}/>
-            {/* <Route path='*' element={alert("404 Not Found")}/> */}
-            <Route path='/welcome' element={<Welcome/>}/>
+            <Route path='*' element={alert("404 Not Found")}/>
+          </Routes> */}
 
-
-      {/* <LoginContainer/> */}
-      {/* <PostList/> */}
       {/* <Home/> */}
-      {/*<Welcome/> */}
-      {/* <Home/> */}
-      {/* <Welcome/> */}
-      {/* <AlterHome/> */}
-
-
-        {/* <PostList/> */}
-        {/* <Home/> */}
-        {/*<Welcome/> */}
-
-        {/* <AlterHome/> */}
-
-          </Routes>
-          
-       {/* <LoginPage/> */}
-        <Home/>
-        {/*<Welcome/> */}
-
-
-        {/* <Profile /> */}
+      <Profile />
+      
     </div>
   );
 }
