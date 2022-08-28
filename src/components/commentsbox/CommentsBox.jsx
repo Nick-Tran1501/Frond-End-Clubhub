68432cfc24830b2a4ee31902d2936e6d71c2e06b
Nick-Tran1501 <channam1501@gmail.com>
2022-08-28 18:30:44 +0700

    update API

--- src/components/commentsbox/CommentsBox.jsx
+++ src/components/commentsbox/CommentsBox.jsx
@@ -1,10 +1,63 @@
-import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 import "./CommentsBox.css";
-import { Avatar, Input, Button } from "antd";
-import  CommentList  from "../commentlist/CommentList";
-const CommentsBox = () => {
+import { Avatar, Input, Button, notification, Space } from "antd";
+import CommentList from "../commentlist/CommentList";
+import axios from "axios";
+
+const CommentsBox = ({ data, postId, userimage }) => {
   const { TextArea } = Input;
+
+  const openNotificationWithIcon = (type) => {
+
+    if(type === "success"){
+      notification["success"]
+      ({
+        message: "Notification Title",
+        description:
+          "Comment Successfully",
+        
+      })
+      
+    }
+    else{
+      notification["error"]
+      ({
+        message: "Notification Title",
+        description:
+          "Please enter your comment",
+      })
+      
+    }
+  };
+
+  const [contents, setContents] = useState("");
+
+  const token = localStorage.getItem("token");
+  const handleComment = (postId) => {
+    axios({
+      headers: { Authorization: `Bearer ${token}` },
+      method: "post",
+      url: "https://rmit-club-dhyty.ondigitalocean.app/api/comment",
+      data: {
+        post: postId,
+        content: contents,
+      },
+    })
+      .then((response) => {
+        console.log(response);
+        return(
+          <Space>
+            {openNotificationWithIcon("success")}
+          </Space>
+        )
+      })
+      .catch((error) => {
+        console.log(error);
+        <Space>
+            {openNotificationWithIcon("error")}
+          </Space>
+      });
+  };
 
   return (
     <React.Fragment>
@@ -13,20 +66,33 @@ const CommentsBox = () => {
           <Avatar
             size={30}
             src={
-              "https://www.24h.com.vn/cong-nghe-thong-tin/da-co-the-tao-den-5-profile-cho-mot-tai-khoan-facebook-c55a1378024.html"
+              userimage
             }
           />
-          <TextArea rows={5} maxLength={500}></TextArea>
+          <TextArea
+            rows={5}
+            maxLength={500}
+            onChange={(e) => {
+              setContents(e.target.value);
+            }}
+            value={contents}
+          ></TextArea>
         </div>
 
         <div className="SubmitButtonContainer">
-          <Button type="primary">
+          <Button
+            type="primary"
+            onClick={() => {
+              handleComment(postId);
+              setContents("")
+            }}
+          >
             Submit
           </Button>
         </div>
-        
-      </div>  
-      <CommentList/>
+      </div>
+
+      <CommentList data={data} />
     </React.Fragment>
   );
 };
