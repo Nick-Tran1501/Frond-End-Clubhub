bab2d402b8afc55fef27e432e84a90a1599b2d5b
Nick-Tran1501 <channam1501@gmail.com>
2022-08-31 13:41:14 +0700

    recommend api

--- src/components/recommendclubs/RecommendClubs.jsx
+++ src/components/recommendclubs/RecommendClubs.jsx
@@ -1,22 +1,50 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Clubs from "./Clubs";
 import "antd/dist/antd.min.css";
 import "./RecommendClubs.css"
+import axios from "axios"
 import { Button } from "antd";
 import {
   ArrowRightOutlined
 } from "@ant-design/icons";
 
 const RecommendClub = () => {
+
+  const [clubInfo,setClubInfo] = useState([
+    {
+      name:"",
+      slogan:"",
+      logoUrl:""
+    }
+  ]
+  )
+  useEffect(() => {
+    axios({
+      method: "get",
+      url: "https://rmit-club-dhyty.ondigitalocean.app/api/clubs/?recruit=true",
+    })
+    .then((res) => {
+      setClubInfo(res.data)
+    })
+    .catch((err) => {console.log(err)})
+  },[])
+  
+  console.log("hihi",clubInfo)
   return(
     <React.Fragment>
 
       <div className="RecommendContainer">
         <h5 className="RecommendTitle"
         >Recommend for you</h5>
-        <Clubs/>
-        <Clubs/>
-        <Clubs/>
+
+        {clubInfo.map((club) => {
+          return(
+
+            <Clubs name={club.name} slogan={club.slogan} logoUrl={club.logoUrl}/>
+          )
+        })}
+
+     
 
 
         <Button
