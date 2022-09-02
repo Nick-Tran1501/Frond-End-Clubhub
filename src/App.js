7efd65ee07e41b18b9d32706f3ba21c44d1f986c
Thang Nguyen <quocthangn1990@gmail.com>
2022-09-02 10:22:06 +0700

    init

--- src/App.js
+++ src/App.js
@@ -16,21 +16,21 @@ function App() {
   return (
     <div className="App">
       {/* Nick's area  */}
-        <Routes>
+      {/* <Routes>
             <Route path='/*' element={<LoginPage/>}/>
 
             <Route path='/home' element={<HomePage/>}/>
             <Route path='/welcome' element={<WelcomePage/>}/>
             <Route path='/profile' element={<ProfilePage/>}/>
-          </Routes>
-    {/* ---------------------------------------------------------------- */}
-      
-          
+          </Routes> */}
+      {/* ---------------------------------------------------------------- */}
+
+
       {/* Tuan's area ---- command if you want do not delete it*/}
 
-            <AdminPage />
-         
-            {/* <HomePage/>
+      <AdminPage />
+
+      {/* <HomePage/>
             <ProfilePage/> */}
       {/* -------------------------------------------------------- */}
 
---
+++ src/pages/president/PresidentPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PresidentPage = () => {
+    return (
+        <div>PresidentPage</div>
+    )
+}
+
+export default PresidentPage
