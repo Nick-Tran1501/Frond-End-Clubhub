2118808ffd72529be84a147fc5f93af83d05944e
Anh Tuan <62132057+Andy1703@users.noreply.github.com>
2022-09-09 20:23:03 +0700

    fix welcomepage

--- src/components/hero/Heros.jsx
+++ src/components/hero/Heros.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Heros.css';
-import Typical from 'react-typical';
+// import Typical from 'react-typical';
+import { TypeAnimation } from 'react-type-animation';
 
 const Hero = () => {
   return (
@@ -11,7 +12,21 @@ const Hero = () => {
           <div className='text-2'>CLUBHUB RMIT VIETNAM</div>
           <div className='text-3'>And we are
             <span>
-              <Typical 
+              <TypeAnimation
+                sequence={[
+                  ' The way finder',
+                  1000,
+                  ' Website Developers', 
+                  1000
+                ]}
+              wrapper="b"
+              cursor={true}
+              repeat={Infinity}
+              style={{ fontSize: '2em' }}
+              />
+            </span>
+            {/* <span> */}
+              {/* <Typical 
               loop={Infinity}
               wrapper="b"
               steps={[
@@ -19,8 +34,8 @@ const Hero = () => {
                 1000,
                 ' Website Developers', 
                 1000
-              ]} />
-            </span>
+              ]} /> */}
+            {/* </span> */}
           </div>
         </div>
       </div>
