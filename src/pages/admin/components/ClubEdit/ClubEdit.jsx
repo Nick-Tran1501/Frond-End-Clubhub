9d1c93cb761e0cbb4ddea75bd262194f829a2071
Anh Tuan <62132057+Andy1703@users.noreply.github.com>
2022-09-10 21:25:11 +0700

    update

--- src/pages/admin/components/ClubEdit/ClubEdit.jsx
+++ src/pages/admin/components/ClubEdit/ClubEdit.jsx
@@ -359,19 +359,19 @@ function ClubEdit() {
           <div className="club-description">
             {clubDisplay && (
               <PageHeader
-                title={clubDisplay.clubData.name}
+                title={clubDisplay?.clubData.name}
                 tags={
-                  <Tag color="blue">{clubDisplay.clubData.clubCategory}</Tag>
+                  <Tag color="blue">{clubDisplay?.clubData.clubCategory}</Tag>
                 }
               >
                 <Row>
                   <Statistic
                     title="President"
-                    value={clubDisplay?.clubData.president.username}
+                    value={clubDisplay?.clubData?.president?.username}
                   />
                   <Statistic
                     title="Members"
-                    value={clubDisplay.memberCount}
+                    value={clubDisplay?.memberCount}
                     style={{
                       margin: "0 50px",
                     }}
@@ -529,7 +529,7 @@ function ClubEdit() {
                       `This student has already been president of another club`
                     )
                   }
-                })
+                });
                 resetEditing();
               }}
             >
