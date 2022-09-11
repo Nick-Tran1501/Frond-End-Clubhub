bfc326e06eccffa7be718827a636dfdc4c9cbab9
Anh Tuan <62132057+Andy1703@users.noreply.github.com>
2022-09-11 16:50:03 +0700

    caigicunglatoi

--- src/components/upcomingevents/UpcomingEvents.jsx
+++ src/components/upcomingevents/UpcomingEvents.jsx
@@ -1,47 +1,110 @@
-import "./UpcomingEvents.css";
-import React from "react";
+import "./UpcomingEvents.scss";
+import React, { useState, useEffect } from "react";
 import "antd/dist/antd.min.css";
-import { Image } from "antd";
+import { Image, Carousel, Typography, Space } from "antd";
+import axios from "axios";
 
-const UpcomingEvent = () => {
+function UpcomingEvent() {
+  const { Title, Text, Paragraph } = Typography;
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    axios({
+      method: "GET",
+      url: "https://rmit-club-dhyty.ondigitalocean.app/api/clubs/events/all",
+    })
+      .then((response) => {
+        setEvents(response.data);
+        console.log("Event", response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  // return events.map((event) => {
   return (
-    <React.Fragment>
-      <div className="upComingContainer">
-        <div className="EventImage">
-          <Image
-            width="100%"
-            src={require("../../image/Image1.jpg")}
-            style={{
-              objectFit: "contain",
-            }}
-          />
-        </div>
-        <div className="EventInfo">
-          <h5
-          style={{
-            fontSize:"15px",
-            fontWeight:"bold",
-            opacity:"0.6"
-          }}
-          >Event</h5>
-          <h5
-            style={{
-              fontSize:"20px",
-              fontWeight:"bold"
-              
-            }}
-          >Club Day</h5>
+    <div className="upComingContainer">
+      {events.length > 0 &&
+        events.map((event) => (
+          <div className="event-card" key={event._id}>
+            <div className="event-pic">
+              <Carousel autoplay dots={false}>
+                {event.imageUrl.map((picture) => {
+                  return <Image key={event.key} height="auto" src={picture} />;
+                })}
+              </Carousel>
+            </div>
+            <div className="event-content">
+              <Title level={5}> {event?.name} </Title>
+              <Text>{event?.location}</Text>
+              <Text>{event?.startDate}</Text>
+            </div>
+          </div>
+        ))}
+
+      {events.length === 0 && <Title level={4}>Events List</Title>}
+    </div>
+
+    // <div className="upComingContainer" key={event._id}>
+    // <div className="EventImage">
+    // <Carousel
+    // >
+
+    /* {event.imageUrl.map((pic) => {
+              return (
+                <Image
+                  key={pic.index}
+                  width="100%"
+                  src={pic}
+                  style={{
+                    objectFit: "contain",
+                  }}
+                  alt="event"
+                />
+              );
+            })} */
+
+    //   </Carousel>
+    // </div>
+
+    /* <div className="EventInfo">
+          <Title
+            level={4}
+            // style={{
+            //   fontSize:"15px",
+            //   fontWeight:"bold",
+            //   opacity:"0.6"
+            // }}
+          >
+            Event
+          </Title>
+
+          <Title
+            Title
+            level={4}
+            // style={{
+            //   fontSize:"20px",
+            //   fontWeight:"bold"
+
+            // }}
+          >
+            {event.name}
+          </Title>
+
           <p
             style={{
-              fontSize:"15px",
-              fontWeight:"bold",
-              opacity:"0.6"
+              fontSize: "15px",
+              fontWeight: "bold",
+              opacity: "0.6",
             }}
-          >Football Club</p>
-        </div>
-      </div>
-    </React.Fragment>
+          >
+            {event._id}
+          </p>
+        </div> */
+    // </div>
   );
-};
+  // });
+}
 
 export default UpcomingEvent;
